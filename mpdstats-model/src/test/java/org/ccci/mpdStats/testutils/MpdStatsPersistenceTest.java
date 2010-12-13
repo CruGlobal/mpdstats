42a99f9162cd13d74459458439b05e094fdbe07e
matthew.drees@uscm.org <matthew.drees@uscm.org@6185d61c-cf3b-044e-8ca2-f4aa9eb47c87>
2010-12-13 15:26:18 +0000

    fix unit tests
    git-svn-id: http://arkham.ccci.org/svn/java/trunk/apps/webapp-mpdstats@7869 6185d61c-cf3b-044e-8ca2-f4aa9eb47c87

--- mpdstats-model/src/test/java/org/ccci/mpdStats/testutils/MpdStatsPersistenceTest.java
+++ mpdstats-model/src/test/java/org/ccci/mpdStats/testutils/MpdStatsPersistenceTest.java
@@ -8,7 +8,7 @@ public class MpdStatsPersistenceTest extends HibernatePersistenceTest
     @Override
     protected String getDefaultPersistenceUnitName()
     {
-        return "MpdStats";
+        return "MpdStatsUnitTest";
     }
 
 }
