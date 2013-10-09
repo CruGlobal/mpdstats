683fd11d39b60f8bb284e193e69c56443cf3e5eb
william.randall@cru.org <william.randall@cru.org@6185d61c-cf3b-044e-8ca2-f4aa9eb47c87>
2013-10-09 17:43:21 +0000

    Update the unit tests to use data providers instead, to reduce copy/paste and to be able to run each test individually rather than if the first test fails, the others get skipped.
    git-svn-id: http://arkham.ccci.org/svn/java/trunk/apps/webapp-mpdstats@14018 6185d61c-cf3b-044e-8ca2-f4aa9eb47c87

--- mpdstats-model/src/test/java/org/ccci/mpdStats/model/ReportPeriodTest.java
+++ mpdstats-model/src/test/java/org/ccci/mpdStats/model/ReportPeriodTest.java
@@ -2,99 +2,66 @@ package org.ccci.mpdStats.model;
 
 import org.joda.time.LocalDate;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class ReportPeriodTest
 {
-    @Test
-    public void testGetReportPeriodEndForDayInPeriod()
+    @DataProvider
+    public Object[][] ValidPeriodEndForDayInPeriodProvider() {
+        return new Object[][] {
+                {new LocalDate(2013, 10, 4), new LocalDate(2013, 10, 5)},
+                {new LocalDate(2013, 10, 5), new LocalDate(2013, 10, 5)},
+                {new LocalDate(2013, 10, 6), new LocalDate(2013, 10, 13)},
+                {new LocalDate(2013, 10, 7), new LocalDate(2013, 10, 13)},
+                {new LocalDate(2013, 10, 8), new LocalDate(2013, 10, 13)},
+                {new LocalDate(2013, 10, 12), new LocalDate(2013, 10, 13)},
+                {new LocalDate(2013, 10, 13), new LocalDate(2013, 10, 13)},
+                {new LocalDate(2013, 10, 14), new LocalDate(2013, 10, 20)},
+                {new LocalDate(2013, 8, 5), new LocalDate(2013, 8, 10)},
+                {new LocalDate(2013, 8, 10), new LocalDate(2013, 8, 10)}
+        };
+    }
+    
+    @DataProvider
+    public Object[][] ValidPreviousReportPeriodProvider() {
+        return new Object[][] {
+                {new LocalDate(2013, 8, 10), new LocalDate(2013, 8, 3)},
+                {new LocalDate(2013, 10, 5), new LocalDate(2013, 9, 28)},
+                {new LocalDate(2013, 10, 13), new LocalDate(2013, 10, 5)},
+                {new LocalDate(2013, 10, 20), new LocalDate(2013, 10, 13)}
+        };
+    }
+    
+    @DataProvider
+    public Object[][] ValidNextReportPeriodProvider() {
+        return new Object[][] {
+                {new LocalDate(2013, 8, 10), new LocalDate(2013, 8, 17)},
+                {new LocalDate(2013, 9, 28), new LocalDate(2013, 10, 5)},
+                {new LocalDate(2013, 10, 5), new LocalDate(2013, 10, 13)},
+                {new LocalDate(2013, 10, 13), new LocalDate(2013, 10, 20)}
+        };
+    }
+    
+    @Test(dataProvider = "ValidPeriodEndForDayInPeriodProvider")
+    public void testGetReportPeriodEndForDayInPeriod(LocalDate localDate, LocalDate expectedReportPeriodEnd)
     {
-        LocalDate localDate = new LocalDate(2013, 10, 4);
         LocalDate reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
-        Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 10, 5));
-        
-        localDate = new LocalDate(2013, 10, 5);
-        reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
-        Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 10, 5));
-        
-        localDate = new LocalDate(2013, 10, 6);
-        reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
-        Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 10, 13));
-        
-        localDate = new LocalDate(2013, 10, 7);
-        reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
-        Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 10, 13));
-        
-        localDate = new LocalDate(2013, 10, 8);
-        reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
-        Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 10, 13));
-        
-        localDate = new LocalDate(2013, 10, 12);
-        reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
-        Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 10, 13));
-        
-        localDate = new LocalDate(2013, 10, 13);
-        reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
-        Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 10, 13));
-        
-        localDate = new LocalDate(2013, 10, 14);
-        reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
-        Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 10, 20));
-        
-        localDate = new LocalDate(2013, 8, 5);
-        reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
-        Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 8, 10));
-        
-        localDate = new LocalDate(2013, 8, 10);
-        reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
-        Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 8, 10));
+        Assert.assertEquals(reportPeriodEnd, expectedReportPeriodEnd);
     }
     
-    @Test
-    public void testPreviousReportPeriod()
+    @Test(dataProvider = "ValidPreviousReportPeriodProvider")
+    public void testPreviousReportPeriod(LocalDate reportPeriodEnd, LocalDate expectedPreviousReportEndDate)
     {
-        LocalDate reportPeriodEnd = new LocalDate(2013, 8, 10);
         ReportPeriod reportPeriod = ReportPeriod.newReportPeriodEndingOn(reportPeriodEnd);
-        ReportPeriod previousPeriod = reportPeriod.previousReportPeriod();
-        Assert.assertEquals(previousPeriod.getEndDate(), new LocalDate(2013, 8, 3));
-        
-        reportPeriodEnd = new LocalDate(2013, 10, 5);
-        reportPeriod = ReportPeriod.newReportPeriodEndingOn(reportPeriodEnd);
-        previousPeriod = reportPeriod.previousReportPeriod();
-        Assert.assertEquals(previousPeriod.getEndDate(), new LocalDate(2013, 9, 28));
-        
-        reportPeriodEnd = new LocalDate(2013, 10, 13);
-        reportPeriod = ReportPeriod.newReportPeriodEndingOn(reportPeriodEnd);
-        previousPeriod = reportPeriod.previousReportPeriod();
-        Assert.assertEquals(previousPeriod.getEndDate(), new LocalDate(2013, 10, 5));
-        
-        reportPeriodEnd = new LocalDate(2013, 10, 20);
-        reportPeriod = ReportPeriod.newReportPeriodEndingOn(reportPeriodEnd);
-        previousPeriod = reportPeriod.previousReportPeriod();
-        Assert.assertEquals(previousPeriod.getEndDate(), new LocalDate(2013, 10, 13));
+        Assert.assertEquals(reportPeriod.previousReportPeriod().getEndDate(), expectedPreviousReportEndDate);
     }
     
-    @Test
-    public void testNextReportPeriod()
+    @Test(dataProvider = "ValidNextReportPeriodProvider")
+    public void testNextReportPeriod(LocalDate reportPeriodEnd, LocalDate expectedNextReportEndDate)
     {
-        LocalDate reportPeriodEnd = new LocalDate(2013, 8, 10);
         ReportPeriod reportPeriod = ReportPeriod.newReportPeriodEndingOn(reportPeriodEnd);
         ReportPeriod nextPeriod = reportPeriod.nextReportPeriod();
-        Assert.assertEquals(nextPeriod.getEndDate(), new LocalDate(2013, 8, 17));
-        
-        reportPeriodEnd = new LocalDate(2013, 9, 28);
-        reportPeriod = ReportPeriod.newReportPeriodEndingOn(reportPeriodEnd);
-        nextPeriod = reportPeriod.nextReportPeriod();
-        Assert.assertEquals(nextPeriod.getEndDate(), new LocalDate(2013, 10, 5));
-        
-        reportPeriodEnd = new LocalDate(2013, 10, 5);
-        reportPeriod = ReportPeriod.newReportPeriodEndingOn(reportPeriodEnd);
-        nextPeriod = reportPeriod.nextReportPeriod();
-        Assert.assertEquals(nextPeriod.getEndDate(), new LocalDate(2013, 10, 13));
-        
-        reportPeriodEnd = new LocalDate(2013, 10, 13);
-        reportPeriod = ReportPeriod.newReportPeriodEndingOn(reportPeriodEnd);
-        nextPeriod = reportPeriod.nextReportPeriod();
-        Assert.assertEquals(nextPeriod.getEndDate(), new LocalDate(2013, 10, 20));
+        Assert.assertEquals(nextPeriod.getEndDate(), expectedNextReportEndDate);
     }
 }
