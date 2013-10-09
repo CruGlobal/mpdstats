afdfffd080020c3d3825f5eef85e3740a91c7501
william.randall@cru.org <william.randall@cru.org@6185d61c-cf3b-044e-8ca2-f4aa9eb47c87>
2013-10-09 13:40:23 +0000

    Add more unit tests for recently changed logic.
    git-svn-id: http://arkham.ccci.org/svn/java/trunk/apps/webapp-mpdstats@14017 6185d61c-cf3b-044e-8ca2-f4aa9eb47c87

--- mpdstats-model/src/test/java/org/ccci/mpdStats/model/ReportPeriodTest.java
+++ mpdstats-model/src/test/java/org/ccci/mpdStats/model/ReportPeriodTest.java
@@ -6,31 +6,30 @@ import org.testng.annotations.Test;
 
 public class ReportPeriodTest
 {
-	@Test
-	public void testGetReportPeriodEndForDayInPeriod()
-	{
-		LocalDate localDate = new LocalDate(2013, 10, 4);
-		
-		LocalDate reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
-		Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 10, 5));
-		
-		localDate = new LocalDate(2013, 10, 5);
-		reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
-		Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 10, 5));
-		
-		localDate = new LocalDate(2013, 10, 6);
-		reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
-		Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 10, 13));
-		
-		localDate = new LocalDate(2013, 10, 7);
-		reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
-		Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 10, 13));
-		
-		localDate = new LocalDate(2013, 10, 8);
-		reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
-		Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 10, 13));
-		
-		localDate = new LocalDate(2013, 10, 12);
+    @Test
+    public void testGetReportPeriodEndForDayInPeriod()
+    {
+        LocalDate localDate = new LocalDate(2013, 10, 4);
+        LocalDate reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
+        Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 10, 5));
+        
+        localDate = new LocalDate(2013, 10, 5);
+        reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
+        Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 10, 5));
+        
+        localDate = new LocalDate(2013, 10, 6);
+        reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
+        Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 10, 13));
+        
+        localDate = new LocalDate(2013, 10, 7);
+        reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
+        Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 10, 13));
+        
+        localDate = new LocalDate(2013, 10, 8);
+        reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
+        Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 10, 13));
+        
+        localDate = new LocalDate(2013, 10, 12);
         reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
         Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 10, 13));
         
@@ -41,5 +40,61 @@ public class ReportPeriodTest
         localDate = new LocalDate(2013, 10, 14);
         reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
         Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 10, 20));
-	}
+        
+        localDate = new LocalDate(2013, 8, 5);
+        reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
+        Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 8, 10));
+        
+        localDate = new LocalDate(2013, 8, 10);
+        reportPeriodEnd = ReportPeriod.getReportPeriodEndForDayInPeriod(localDate);
+        Assert.assertEquals(reportPeriodEnd, new LocalDate(2013, 8, 10));
+    }
+    
+    @Test
+    public void testPreviousReportPeriod()
+    {
+        LocalDate reportPeriodEnd = new LocalDate(2013, 8, 10);
+        ReportPeriod reportPeriod = ReportPeriod.newReportPeriodEndingOn(reportPeriodEnd);
+        ReportPeriod previousPeriod = reportPeriod.previousReportPeriod();
+        Assert.assertEquals(previousPeriod.getEndDate(), new LocalDate(2013, 8, 3));
+        
+        reportPeriodEnd = new LocalDate(2013, 10, 5);
+        reportPeriod = ReportPeriod.newReportPeriodEndingOn(reportPeriodEnd);
+        previousPeriod = reportPeriod.previousReportPeriod();
+        Assert.assertEquals(previousPeriod.getEndDate(), new LocalDate(2013, 9, 28));
+        
+        reportPeriodEnd = new LocalDate(2013, 10, 13);
+        reportPeriod = ReportPeriod.newReportPeriodEndingOn(reportPeriodEnd);
+        previousPeriod = reportPeriod.previousReportPeriod();
+        Assert.assertEquals(previousPeriod.getEndDate(), new LocalDate(2013, 10, 5));
+        
+        reportPeriodEnd = new LocalDate(2013, 10, 20);
+        reportPeriod = ReportPeriod.newReportPeriodEndingOn(reportPeriodEnd);
+        previousPeriod = reportPeriod.previousReportPeriod();
+        Assert.assertEquals(previousPeriod.getEndDate(), new LocalDate(2013, 10, 13));
+    }
+    
+    @Test
+    public void testNextReportPeriod()
+    {
+        LocalDate reportPeriodEnd = new LocalDate(2013, 8, 10);
+        ReportPeriod reportPeriod = ReportPeriod.newReportPeriodEndingOn(reportPeriodEnd);
+        ReportPeriod nextPeriod = reportPeriod.nextReportPeriod();
+        Assert.assertEquals(nextPeriod.getEndDate(), new LocalDate(2013, 8, 17));
+        
+        reportPeriodEnd = new LocalDate(2013, 9, 28);
+        reportPeriod = ReportPeriod.newReportPeriodEndingOn(reportPeriodEnd);
+        nextPeriod = reportPeriod.nextReportPeriod();
+        Assert.assertEquals(nextPeriod.getEndDate(), new LocalDate(2013, 10, 5));
+        
+        reportPeriodEnd = new LocalDate(2013, 10, 5);
+        reportPeriod = ReportPeriod.newReportPeriodEndingOn(reportPeriodEnd);
+        nextPeriod = reportPeriod.nextReportPeriod();
+        Assert.assertEquals(nextPeriod.getEndDate(), new LocalDate(2013, 10, 13));
+        
+        reportPeriodEnd = new LocalDate(2013, 10, 13);
+        reportPeriod = ReportPeriod.newReportPeriodEndingOn(reportPeriodEnd);
+        nextPeriod = reportPeriod.nextReportPeriod();
+        Assert.assertEquals(nextPeriod.getEndDate(), new LocalDate(2013, 10, 20));
+    }
 }
